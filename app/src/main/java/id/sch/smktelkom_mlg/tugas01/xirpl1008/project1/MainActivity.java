b1e0a6622eddebe7e1d0987cf0f117eb61edddd6
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2016-09-10 10:03:31 +0700

    Commit Pertama Nama dan Asal

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1008/project1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1008/project1/MainActivity.java
@@ -2,12 +2,65 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1008.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    EditText etAsal;
+    Button bOK;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etAsal = (EditText) findViewById(R.id.editTextAsal);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+            }
+        });
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String asal = etAsal.getText().toString();
+            tvHasil.setText(nama + " /n " + asal);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String asal = etAsal.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        if (asal.isEmpty()) {
+            etAsal.setError("Asal SMP belum diisi");
+            valid = false;
+        } else {
+            etAsal.setError(null);
+        }
+        return valid;
     }
 }
