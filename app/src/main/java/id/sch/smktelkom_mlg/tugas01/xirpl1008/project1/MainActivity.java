f455d6afb77f8e1ff151fffc9cf111740aece0c7
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2016-09-10 12:38:00 +0700

    Commit Pertama Nama dan Asal Commit Kedua memberi spinner dan radiobutton

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1008/project1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1008/project1/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etNama;
     EditText etAsal;
+    RadioGroup rgJK;
+    Spinner spJurusan;
     Button bOK;
     TextView tvHasil;
 
@@ -19,48 +24,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         etNama = (EditText) findViewById(R.id.editTextNama);
         etAsal = (EditText) findViewById(R.id.editTextAsal);
+        rgJK = (RadioGroup) findViewById(R.id.radioGroupJK);
+        spJurusan = (Spinner) findViewById(R.id.spinnerProvinsi);
         bOK = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        bOK.setOnClickListener(new View.OnClickListener()
 
-        bOK.setOnClickListener(new View.OnClickListener() {
+        {
             @Override
             public void onClick(View view) {
-                doProcess();
+                doClick();
             }
         });
     }
 
-    private void doProcess() {
-        if (isValid()) {
-            String nama = etNama.getText().toString();
-            String asal = etAsal.getText().toString();
-            tvHasil.setText(nama + " /n " + asal);
-        }
-    }
-
-    private boolean isValid() {
-        boolean valid = true;
-
+    private void doClick() {
         String nama = etNama.getText().toString();
         String asal = etAsal.getText().toString();
+        String jurusan = spJurusan.getSelectedItem().toString();
+        String hasil = null;
 
         if (nama.isEmpty()) {
             etNama.setError("Nama belum diisi");
-            valid = false;
         } else if (nama.length() < 3) {
             etNama.setError("Nama minimal 3 karakter");
-            valid = false;
         } else {
             etNama.setError(null);
         }
         if (asal.isEmpty()) {
             etAsal.setError("Asal SMP belum diisi");
-            valid = false;
         } else {
             etAsal.setError(null);
         }
-        return valid;
+        if (rgJK.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgJK.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+        }
+
+        tvHasil.setText("Nama : " + nama + "\n" + "Asal SMP : " + asal + "\n" +
+                "Jenis Kelamin : " + hasil + "\nJurusan : " + jurusan);
     }
 }
